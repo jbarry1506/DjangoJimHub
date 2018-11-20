eebd5d35a5ceab315c56937fb9fa8512c39717bd
Jim Barry <jbarry1506@gmail.com>
2018-11-20 15:44:21 -0500

    added logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,3 +1,4 @@
+
 from . import views
 
 urlpatterns = [
--- jimhub/urls.py
+++ jimhub/urls.py
@@ -13,8 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.contrib import admin, auth
+from django.urls import path, include
 from django.views.generic.base import TemplateView
 from . import views
 from accounts import urls
@@ -22,4 +22,5 @@ from accounts import urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.IndexView.as_view(), name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
