fbd38ab7ebacb8195fc9c68f713379e886c142da
Jim Barry <jbarry1506@gmail.com>
2018-11-20 14:53:06 -0500

    added login page

--- jimhub/urls.py
+++ jimhub/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.views.generic.base import TemplateView
 from . import views
+from accounts import urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
